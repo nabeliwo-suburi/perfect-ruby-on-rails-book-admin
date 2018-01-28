d2cc00d4e1e9a2c583e64722fbaa59be0d619958
nabeliwo <hiroki.c.watanabe@gmail.com>
2018-01-28 20:49:46 +0900

    add book model

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, -> (theme) { where("name like ?", "%#{theme}%") }
+
+  default_scope -> { order("published_on desc") }
+end
